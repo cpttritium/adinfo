b07b9262ef850482363f055ab2ae093c8cada033
Tyler Ulibarri <tulibarri@crmcinc.org>
2017-11-10 10:06:05 -0600

    First

---
+++ Get-ADInfo.ps1
@@ -0,0 +1,14 @@
+﻿param(
+    [string]$UserID
+)
+$UserID = "tulibarri"
+$UserData = Get-ADuser -Identity $UserID -Properties *
+$UserGroup = Get-ADPrincipalGroupMembership -Identity $UserID | Select-Object Name
+
+$UserAccount = $UserData.SamAccountName
+$Name = $UserData.Name
+$Active = $UserData.Enabled
+$LastLogon = $UserData.LastLogonDate
+
+Write-Host $UserAccount, $Name, $Active, $LastLogon
+Write-Host $UserGroup
